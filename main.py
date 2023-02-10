eaff9dcfa97738086d887e306c78ed97bbb5c0f7
Nicolas Kohl <nicolas.kohl@sap.com>
2023-02-10 15:15:14 +0100

    Add: first endpoints

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    rt_url: str
+    yt_url: str | None = None
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/config")
+async def video_config(config: Config):
+    return config
+
+
+@app.get("/downloadVideo")
+async def download_video():
+    return "This is the video"
